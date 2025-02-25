9ac2d38cbec63289eecfb3c52c0a4797d81dfb71
Suneel Belkhale <neelus.genius@gmail.com>
2025-02-25 15:32:28 -0800

    slider basics

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
+// Next/previous controls
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+let i;
+let slides = document.getElementsByClassName("video-container");
+let dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+}
+
+let slideIndex = 1;
 
 $(document).ready(function() {
     var editor = CodeMirror.fromTextArea(document.getElementById("bibtex"), {
@@ -37,4 +64,7 @@ $(document).ready(function() {
     
     
 //     window.requestAnimationFrame(scrollPlay);
+
+  showSlides(slideIndex);
+  
 });
